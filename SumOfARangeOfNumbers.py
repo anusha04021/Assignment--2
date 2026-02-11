70abe3eaa7696e34446eb252f5e383abb53c6eb0
anusha04021 <71581577+anusha04021@users.noreply.github.com>
2026-02-11 11:09:53 +0530

    Add files via upload

---
+++ SumOfARangeOfNumbers.py
@@ -0,0 +1,10 @@
+# Initialize a variable to store the sum
+total_sum = 0
+
+# Use a for loop to iterate over numbers from 1 to 50 (inclusive)
+for num in range(1, 51):
+    # Add the current number to the total_sum
+    total_sum += num
+
+# Display the final sum
+print("The sum of numbers from 1 to 50 is:", total_sum)
